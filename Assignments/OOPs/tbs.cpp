9cc7a0e967c9d5a317394321a3ada8287a0659cf
Dhruv Mittal <dhruvm22@iitk.ac.in>
2023-05-18 16:19:22 +0530

    registration portal made

--- Assignments/OOPs/tbs.cpp
+++ Assignments/OOPs/tbs.cpp
@@ -2,28 +2,47 @@
 using namespace std;
 class user
 {
-public:
+private:
     string username;
     string password;
     string name;
     string list_of_tickets;
-
+public:
     user(string Username,
-    string Password,
-    string Name,
-    string List_of_tickets,
-) 
+         string Password,
+         string Name,
+         string List_of_tickets,
+        ) 
     {
         username = Username;
         password = Password;
         name = Name;
-        list_of_tickets = List_of_tickets;
-         
-           
+        list_of_tickets = List_of_tickets;         
     }
+    void register()
+    { 
+        string Name,Username,Password;
+        cout>> "Enter Name\n";
+        cin<<"Name";
+        cout>>"Enter username\n";
+        cin<<"Username";
+        cout<<"Enter password\n";
+        cin>>"Password";
+        user(Username,Password,Name,List_of_tickets);
+        
+    }
+    // void setName(string Name)
+    // {
+    //     name = Name;
+    // }
+
+    
+    
 };
 int main() 
+
 {
+    int flag = 0;
     user dhruv = user("dhruvmittal","dhruvmittal","Dhruv","kanpur to delhi");
 
 }

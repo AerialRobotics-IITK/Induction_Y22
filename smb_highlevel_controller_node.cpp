9b1fc203c17febbd940c56108c47acf44bd8705f
Shubh_Jain <121512095+Shubh152@users.noreply.github.com>
2023-06-12 11:18:12 +0530

    Update ROS_Week2_Assignment
    this is the final submission rectifying the previous log error
    the target executable had a wrong RUNTIME_DESTINATION
    fixed CMakelists.txt

--- smb_highlevel_controller_node.cpp
+++ smb_highlevel_controller_node.cpp
@@ -1,12 +1,28 @@
-#include <smb_highlevel_controller/SmbHighlevelController.hpp>
+#include <iostream>
+#include <ros/ros.h>
+// #include <smb_highlevel_controller/SmbHighlevelController.hpp>
 #include <sensor_msgs/LaserScan.h>
+// #include <ros/rosconsole.h>
+#include <bits/stdc++.h>
+#include <vector>
+#include <limits>
+
+void scanCallBack(const sensor_msgs::LaserScan::ConstPtr& msg){
+
+    std::vector<float> range = msg->ranges;
+    float temp = std::numeric_limits<float>::max();
+    for(auto& itr : range){
+            if(itr < temp) temp = itr;
+    }
+    ROS_INFO("Minimum distance = %.2f", temp);
+
+}
 
 int main(int argc, char** argv){
 
     ros::init(argc, argv, "Scanner");
     ros::NodeHandle nh;
-    smb_highlevel_controller::SmbHighlevelController shc(nh);
-    ros::Subscriber scanSub = (nh).subscribe("scan", 10, &smb_highlevel_controller::SmbHighlevelController::scanCallBack,&shc);
+    ros::Subscriber scanSub = (nh).subscribe("scan", 5,scanCallBack);
     ros::spin();
     return 0;
 }
